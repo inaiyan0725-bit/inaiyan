dc4665cbc80bbdf4b73779954189ea957ac2ab6b
inaiyan0725-bit <inaiyan0725@gmail.com>
2026-05-03 16:40:58 +0530

    Update get-orders.js

--- api/get-orders.js
+++ api/get-orders.js
@@ -1,7 +1,8 @@
 export default async function handler(req, res) {
   // Allow only POST
 export default function handler(req, res) {
-  res.status(200).json({ message: "API working 🚀" });
+  return res.send("OK");
+}
 }
 
   const { phone } = req.body;
